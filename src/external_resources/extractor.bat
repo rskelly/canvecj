b5e67fee33bfeef67efac7c7add666cb7ea0e330
Rob <rob@dijital.ca>
2013-07-05 21:22:26 -0700

    Added echo off to batch file.

--- src/external_resources/extractor.bat
+++ src/external_resources/extractor.bat
@@ -1,19 +1,34 @@
+@echo off
 rem This script runs the CanVec extractor using the arguments
-rem configured below, and send the output to stdout.
+rem configured below, and sends the output to stdout.
+
 rem You can pipe the output directly into PostGIS like so:
+
 rem
+
 rem ./extractor.bat|psql -d mydatabase
+
 rem
+
 rem Assuming you run the extractor as a user with privileges on the 
+
 rem database.
 
+
 rem Limiting the JVM's heap allows it to fork when it invokes
+
 rem the shp2pgsql program.
+
+
 
 set JAVA_OPTS=-Xms64m -Xmx128m
 
+
 set EXTRACTOR_JAR=canvec_extractor.jar
+
 set EXTRACTOR_JOBS=extractor.jobs
+
+
 
 java %JAVA_OPTS% -jar %EXTRACTOR_JAR% %EXTRACTOR_JOBS% -
 
